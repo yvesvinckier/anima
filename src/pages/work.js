52cc85ab0dd8e7fb739e5240ff65b8fa5d600c29
yves <yves.vinckier@live.fr>
2019-08-27 15:55:00 +0200

    default work.js

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,3 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/general/Layout'
+import SEO from '../components/general/SEO'
+
+const WorkPage = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Hi from the WorkPage</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default WorkPage
+
 // import { TextureLoader } from 'three'
 // import { useSpring, animated, config } from 'react-spring/three'
 // import React, { useState, useMemo } from 'react'
